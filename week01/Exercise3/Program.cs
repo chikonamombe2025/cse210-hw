b7924ec0034ea6f72f62c53c265319c2993e1a8a
chikonamombe2025 <jonahondoyedzomba@yahoo.com>
2025-11-08 13:44:20 +0200

    Refactor guessing game logic with do-while loop

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,33 +1,54 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
 
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101); // Random number between 1 and 100
-
-        int guess = 0;
-
-        // Step 4: Loop until the guess is correct
-        while (guess != magicNumber)
+        do
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101); // Random number between 1 and 100
 
-            if (guess < magicNumber)
+            int guess = 0;
+            int numberOfGuesses = 0;
+            List<int> previousGuesses = new List<int>(); // To store previous guesses
+
+            // While loop to keep guessing until correct
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                numberOfGuesses++;
+                previousGuesses.Add(guess); // Store the guess
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+                }
             }
-            else if (guess > magicNumber)
+
+            // For loop to display previous guesses
+            Console.WriteLine("Previous guesses:");
+            for (int i = 0; i < previousGuesses.Count; i++)
             {
-                Console.WriteLine("Lower");
+                Console.WriteLine(previousGuesses[i]);
             }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            } 
+
+            // Do-While loop to ask if they want to play again
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
     }
 }
