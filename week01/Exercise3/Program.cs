121598c0e0f428270073446562355a06b5926160
chikonamombe2025 <jonahondoyedzomba@yahoo.com>
2025-11-08 13:21:40 +0200

    Implement magic number guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); // Random number between 1 and 100
+
+        int guess = 0;
+
+        // Step 4: Loop until the guess is correct
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            } 
     }
 }
