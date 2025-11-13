f31c88d9a51899db446edbaf27021011ca22a992
chikonamombe2025 <jonahondoyedzomba@yahoo.com>
2025-11-13 07:14:58 +0200

    Implement Journal class with entry management

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> _entries;
+
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (var writer = new StreamWriter(filename))
+        {
+            foreach (var entry in _entries)
+            {
+                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear(); // Clear existing entries
+        using (var reader = new StreamReader(filename))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    _entries.Add(new Entry(parts[1], parts[2], parts[0]));
+                }
+            }
+        }
+    }
+}
