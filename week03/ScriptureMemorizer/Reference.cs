9a5824c64b9ae04a58bcd9a1055af8b8e824fa7f
chikonamombe2025 <jonahondoyedzomba@yahoo.com>
2025-11-16 01:13:30 +0200

    Add Reference class for scripture memorization

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,18 @@
+public class Reference
+{
+    public string Book { get; private set; }
+    public int Chapter { get; private set; }
+    public int Verse { get; private set; }
+
+    public Reference(string book, int chapter, int verse)
+    {
+        Book = book;
+        Chapter = chapter;
+        Verse = verse;
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{Book} {Chapter}:{Verse}";
+    }
+}
