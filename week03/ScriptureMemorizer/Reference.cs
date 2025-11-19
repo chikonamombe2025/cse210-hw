4c78d8f8e29bae49991cf667bbc17a82fbf3308a
chikonamombe2025 <jonahondoyedzomba@yahoo.com>
2025-11-19 16:52:36 +0200

    Refactor Reference class to support multiple verses

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -4,7 +4,24 @@ public class Reference
     public int Chapter { get; private set; }
     public int Verse { get; private set; }
 
-    public Reference(string book, int chapter, int verse)
+    public Reference(strpublic class Reference
+{ 
+    private string Book { get; }
+    private int Chapter { get; }
+    private List<int> Verses { get; }
+
+    public Reference(string book, int chapter, List<int> verses)
+    {
+        Book = book;
+        Chapter = chapter;
+        Verses = verses;
+    }
+
+    public override string ToString()
+    {
+        return $"{Book} {Chapter}:{string.Join("-", Verses)}";
+    }
+}ing book, int chapter, int verse)
     {
         Book = book;
         Chapter = chapter;
