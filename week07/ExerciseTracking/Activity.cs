93e59ad53b45754e851baaa2e07829dbeaa3e1d8
chikonamombe2025 <jonahondoyedzomba@yahoo.com>
2025-12-07 03:52:32 +0200

    Add abstract Activity class for exercise tracking

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,21 @@
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetType().Name} ({_minutes} min) - " +
+               $"Distance: {GetDistance():F1}, Speed: {GetSpeed():F1}, Pace: {GetPace():F1}";
+    }
+}
